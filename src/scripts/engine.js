17fe46b762798e564c95bea613c858af745d9c02
DRISSA CAS <drissa_cas@hotmail.com>
2024-11-24 23:38:29 -0300

    Upgrade in challenge

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -16,8 +16,8 @@ const emojis = [
     "😻",
     "😻"
 ];
-let openCards = [];
 
+let openCards = [];
 
 let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
 
@@ -29,29 +29,37 @@ for(let i = 0; i < emojis.length; i++){
     document.querySelector(".game").appendChild(box);
 }
 
+function playSound(audioName) {
+    let audio = new Audio(`./src/audios/${audioName}.m4a`);
+    audio.volume = 0.2;
+    audio.play();
+}
+
 function handleClick() {
     if(openCards.length < 2){
         this.classList.add("boxOpen");
         openCards.push(this);
     }
-
+    
     if(openCards.length == 2){
         setTimeout(checkMatch, 500);
     }
 }
 
+
 function checkMatch() {
     if(openCards[0].innerHTML === openCards[1].innerHTML) {
         openCards[0].classList.add("boxMatch");
         openCards[1].classList.add("boxMatch");
+        playSound("hit");
     } else {
         openCards[0].classList.remove("boxOpen");
         openCards[1].classList.remove("boxOpen");
     }
-
+    
     openCards = [];
 
     if(document.querySelectorAll(".boxMatch").length === emojis.length) {
-        alert("Parabéns! Você acertou todos os pares.")
+        alert("Parabéns! Você acertou todos os pares.");
     }
 }
